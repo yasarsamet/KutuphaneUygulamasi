0a362f7dfa3a23e6f610791cba4a5d68787e8954
yasarsametalic <samet@oceannet.com.tr>
2021-02-03 14:13:41 +0300

    Update

--- Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
+++ Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -44,7 +44,7 @@ namespace Core.DataAccess.EntityFramework
         {
             using (var context = new TContext())
             {
-                return filter == null
+                return filter == null   
                 ? context.Set<TEntity>().ToList()
                 : context.Set<TEntity>().Where(filter).ToList();
             }
--- Entities/Concrete/Ogrenci.cs
+++ Entities/Concrete/Ogrenci.cs
@@ -12,14 +12,13 @@ namespace Entities.Concrete
         public string Isim { get; set; }
         public string Soyisim { get; set; }
         public string Telefon { get; set; }
-        public string Email { get; set; }        
-        public string DogumTarihi {get;set;}
+        public string Email { get; set; }
+        public string DogumTarihi { get; set; }
         public string Okulu { get; set; }
         public string Sinifi { get; set; }
         public string Durumu { get; set; }
-        public string Adres{ get; set; }
+        public string Adres { get; set; }
         public string Sifre { get; set; }
-        //public string 
-
+        public string Role { get; set; }
     }
 }
